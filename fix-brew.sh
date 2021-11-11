e5b9a91c490174c7f08bc20c1a2bfe84fc727bda
Cornelius Toole <cornelius.toole@gmail.com>
2021-11-10 19:41:13 -0600

    my customizations
      - .extra most of my custom configs, aliases, functions, etc.
      - brew bundle file and fixes for the upstream brew.sh script

---
+++ fix-brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+brew uninstall -f aircrack-ng
+brew uninstall -f bfg
+brew uninstall -f binutils
+brew uninstall -f binwalk
+brew uninstall -f cifer
+brew uninstall -f dex2jar
+brew uninstall -f dns2tcp
+brew uninstall -f fcrackzip
+brew uninstall -f foremost
+brew uninstall -f hashpump
+brew uninstall -f hydra
+brew uninstall -f john
+brew uninstall -f knock
+brew uninstall -f netpbm
+brew uninstall -f nmap
+brew uninstall -f pngcheck
+brew uninstall -f socat
+brew uninstall -f sqlmap
+brew uninstall -f tcpflow
+brew uninstall -f tcpreplay
+brew uninstall -f tcptrace
+brew uninstall -f ucspi-tcp # `tcpserver` etc.
+brew uninstall -f xpdf
+brew uninstall -f xz
